d42c8e8d036d6a418ec3c366d21e40dbdde2e236
szhuravlev <gcrane@bk.ru>
2023-03-24 01:28:22 +0300

    fixed tests

--- src/test/java/dev/greycrane/tests/BrowserstackSearchTests.java
+++ src/test/java/dev/greycrane/tests/BrowserstackSearchTests.java
@@ -1,5 +1,6 @@
 package dev.greycrane.tests;
 
+import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +9,8 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
+import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
-import static jdk.nashorn.internal.runtime.Debug.id;
 
 @Tag("mobile")
 public class BrowserstackSearchTests extends TestBase {
